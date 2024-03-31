8746aeadcbbc3430eb56da32e9dad479ba59bcf8
Helw150 <wbh230@nyu.edu>
2024-03-30 23:25:54 -0400

    Decoder Finetune

--- src/levanter/models/via.py
+++ src/levanter/models/via.py
@@ -191,11 +191,12 @@ class ViaModel(eqx.Module, ModelWithHfSerializationMixin[ViaConfig]):
             audio_features,  # causal_mask,
             key=k_connector,
         )
-        soft_whisper_logits = self.connector.embeddings.unembed(virt_whisper_tokens)
+        soft_whisper_logits = hax.nn.softmax(
+            self.connector.embeddings.unembed(virt_whisper_tokens), axis=self.connector.Vocab
+        )
 
-        lm_logits = soft_whisper_logits
-        # virtual_tokens = self.projection(soft_whisper_logits)
-        # lm_logits = self.decoder.embeddings.unembed(virtual_tokens)
+        virtual_tokens = self.projection(soft_whisper_logits)
+        lm_logits = self.decoder.embeddings.unembed(virtual_tokens)
         return lm_logits["position", : input_ids.resolve_axis("position").size]
         # # Embed Real LLM Tokens
         # prefix = self.decoder.embeddings.embed(self.config.prefix)
